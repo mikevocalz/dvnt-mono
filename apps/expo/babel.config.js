07e38a8b850fc3af8f6630c8c40bb8e80ac79d42
Michael Allen <mikefacesny@gmail.com>
2025-12-26 14:21:36 -0500

    Add shared ui aliases and app structure

--- apps/expo/babel.config.js
+++ apps/expo/babel.config.js
@@ -1,7 +1,23 @@
+const path = require('path')
+
 module.exports = function (api) {
   api.cache(true)
   return {
     presets: [['babel-preset-expo', { jsxRuntime: 'automatic' }]],
-    plugins: ['react-native-worklets/plugin'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          alias: {
+            '@ui': path.resolve(__dirname, '../../packages/app/ui'),
+            '@store': path.resolve(__dirname, '../../packages/app/store'),
+            '@services': path.resolve(__dirname, '../../packages/app/services'),
+            '@utils': path.resolve(__dirname, '../../packages/app/utils'),
+            '@hooks': path.resolve(__dirname, '../../packages/app/hooks'),
+          },
+        },
+      ],
+      'react-native-worklets/plugin',
+    ],
   }
 }
---
+++ packages/app/ui/text.tsx
@@ -0,0 +1,3 @@
+import { Text as ReusablesText } from 'react-native-reusables'
+
+export { ReusablesText as Text }
